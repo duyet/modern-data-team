25c13ae79eb1317d4bed51cfc93522a60be4822c
Duyet Le <lvduyet@fossil.com>
2023-01-08 17:29:50 +0700

    feat: initial

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Modern Data Team</span>,
+  project: {
+    link: 'https://github.com/duyet/modern-data-team',
+  },
+  docsRepositoryBase: 'https://github.com/duyet/modern-data-team',
+  footer: {
+    text: 'MIT 2023 © Modern Data Team',
+  },
+}
+
+export default config
